1a96163e18ac216999512b95c9a761d1027a8701
akshay-1705 <akshay.arora1705@gmail.com>
2023-09-02 17:51:46 +0530

    add sign in and forgot password screens

---
+++ lib/login/reset_password.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class ResetPasswordWidget extends StatefulWidget {
+  const ResetPasswordWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ResetPasswordWidget> createState() => _ResetPasswordWidgetState();
+}
+
+class _ResetPasswordWidgetState extends State<ResetPasswordWidget> {
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController nameController = TextEditingController();
+    return Scaffold(
+      body: showModal(nameController),
+    );
+  }
+
+  showModal(TextEditingController nameController) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.black),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ),
+          const SizedBox(height: 65),
+          Align(
+              alignment: Alignment.center,
+              child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                        color: Color(0xff000000),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 30),
+                  ))),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: const Text(
+              "Enter the email address you've used to register with us and we'll send you a reset link!",
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(height: 40),
+          Opacity(
+              opacity: 0.3,
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    labelText: 'Email*',
+                  ),
+                ),
+              )),
+          const SizedBox(height: 20),
+          Container(
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    // print(nameController.text);
+                    // print(passwordController.text);
+                  },
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            side: const BorderSide(color: Color(0xff2cc48a))),
+                      ),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color(0xff2cc48a))),
+                  child: const Text('Forgot',
+                      style: TextStyle(color: Colors.white)))),
+        ]));
+  }
+}
---
+++ lib/login/sign_in.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:maison_mate/login/reset_password.dart';
+
+class SignInWidget extends StatefulWidget {
+  const SignInWidget({Key? key}) : super(key: key);
+
+  @override
+  State<SignInWidget> createState() => _SignInWidgetState();
+}
+
+class _SignInWidgetState extends State<SignInWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            const SizedBox(height: 60),
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Sign In',
+                  style: TextStyle(
+                      color: Color(0xff000000),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 30),
+                )),
+            Container(
+              alignment: Alignment.center,
+              child: const Text('To access the portal'),
+            ),
+            const SizedBox(height: 15),
+            Opacity(
+                opacity: 0.3,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8))),
+                      labelText: 'Email*',
+                    ),
+                  ),
+                )),
+            Opacity(
+              opacity: 0.3,
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: TextField(
+                  obscureText: true,
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    labelText: 'Password*',
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ResetPasswordWidget()),
+                );
+              },
+              child: const Text('Forgot Password?',
+                  style: TextStyle(color: Colors.black)),
+            ),
+            const SizedBox(height: 10),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
+                    onPressed: () {
+                      // print(nameController.text);
+                      // print(passwordController.text);
+                    },
+                    style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              side: const BorderSide(color: Color(0xff2cc48a))),
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            const Color(0xff2cc48a))),
+                    child: const Text('Login',
+                        style: TextStyle(color: Colors.white)))),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text("Don't have an account?"),
+                TextButton(
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        fontSize: 17, color: Colors.black.withOpacity(0.6)),
+                  ),
+                  onPressed: () {
+                    //signup screen
+                  },
+                )
+              ],
+            ),
+          ],
+        ));
+  }
+}
