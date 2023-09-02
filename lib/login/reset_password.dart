7b8b46ff9d35f03a378e3a3938f480c6686ac591
akshay-1705 <akshay.arora1705@gmail.com>
2023-09-02 23:50:02 +0530

    use GestureDetector

--- lib/login/reset_password.dart
+++ lib/login/reset_password.dart
@@ -17,69 +17,76 @@ class _ResetPasswordWidgetState extends State<ResetPasswordWidget> {
   }
 
   renderModal(TextEditingController emailController) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(children: [
-          Align(
-            alignment: Alignment.topLeft,
-            child: IconButton(
-              icon: const Icon(Icons.arrow_back, color: Colors.black),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-          ),
-          const SizedBox(height: 65),
-          Align(
-              alignment: Alignment.center,
-              child: Container(
-                  padding: const EdgeInsets.all(10),
-                  child: const Text(
-                    'Forgot Password?',
-                    style: TextStyle(
-                        color: Color(0xff000000),
-                        fontWeight: FontWeight.w400,
-                        fontSize: 30),
-                  ))),
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: const Text(
-              "Enter the email address you've used to register with us and we'll send you a reset link!",
-              textAlign: TextAlign.center,
-            ),
-          ),
-          const SizedBox(height: 40),
-          Opacity(
-              opacity: 0.3,
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8))),
-                    labelText: 'Email*',
-                  ),
+    return GestureDetector(
+        onTap: () {
+          // Dismiss the keyboard when tapped on a non-actionable item
+          FocusScope.of(context).unfocus();
+        },
+        child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back, color: Colors.black),
+                  onPressed: () => Navigator.of(context).pop(),
                 ),
-              )),
-          const SizedBox(height: 20),
-          Container(
-              height: 50,
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: ElevatedButton(
-                  onPressed: () {
-                    // print(emailController.text);
-                    // print(passwordController.text);
-                  },
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            side: const BorderSide(color: Color(0xff2cc48a))),
+              ),
+              const SizedBox(height: 65),
+              Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                      padding: const EdgeInsets.all(10),
+                      child: const Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                            color: Color(0xff000000),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 30),
+                      ))),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: const Text(
+                  "Enter the email address you've used to register with us and we'll send you a reset link!",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(height: 40),
+              Opacity(
+                  opacity: 0.3,
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: TextField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8))),
+                        labelText: 'Email*',
                       ),
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xff2cc48a))),
-                  child: const Text('Forgot',
-                      style: TextStyle(color: Colors.white)))),
-        ]));
+                    ),
+                  )),
+              const SizedBox(height: 20),
+              Container(
+                  height: 50,
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        // print(emailController.text);
+                        // print(passwordController.text);
+                      },
+                      style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                side:
+                                    const BorderSide(color: Color(0xff2cc48a))),
+                          ),
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xff2cc48a))),
+                      child: const Text('Forgot',
+                          style: TextStyle(color: Colors.white)))),
+            ])));
   }
 }
--- lib/login/sign_up.dart
+++ lib/login/sign_up.dart
@@ -22,170 +22,177 @@ class _SignUpWidgetState extends State<SignUpWidget> {
     );
   }
 
-  Padding renderModal(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(children: [
-          const SizedBox(height: 80),
-          Align(
-              alignment: Alignment.center,
-              child: Container(
-                  padding: const EdgeInsets.all(10),
-                  child: const Text(
-                    'Sign Up',
-                    style: TextStyle(
-                        color: Color(0xff000000),
-                        fontWeight: FontWeight.w400,
-                        fontSize: 30),
-                  ))),
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: const Text(
-              "To access the portal",
-              textAlign: TextAlign.center,
-            ),
-          ),
-          const SizedBox(height: 15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Flexible(
-                child: Opacity(
-                    opacity: 0.3,
-                    child: Container(
+  renderModal(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          // Dismiss the keyboard when tapped on a non-actionable item
+          FocusScope.of(context).unfocus();
+        },
+        child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(children: [
+              const SizedBox(height: 80),
+              Align(
+                  alignment: Alignment.center,
+                  child: Container(
                       padding: const EdgeInsets.all(10),
-                      child: TextField(
-                        controller: firstNameController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8))),
-                          labelText: 'First Name',
-                        ),
-                      ),
-                    )),
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            color: Color(0xff000000),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 30),
+                      ))),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: const Text(
+                  "To access the portal",
+                  textAlign: TextAlign.center,
+                ),
               ),
-              Flexible(
-                child: Opacity(
-                    opacity: 0.3,
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      child: TextField(
-                        controller: lastNameController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8))),
-                          labelText: 'Last Name',
-                        ),
-                      ),
-                    )),
-              )
-            ],
-          ),
-          Opacity(
-              opacity: 0.3,
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8))),
-                    labelText: 'Email*',
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Flexible(
+                    child: Opacity(
+                        opacity: 0.3,
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          child: TextField(
+                            controller: firstNameController,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8))),
+                              labelText: 'First Name',
+                            ),
+                          ),
+                        )),
                   ),
-                ),
-              )),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Flexible(
-                child: Opacity(
-                    opacity: 0.3,
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      child: TextField(
-                        controller: passwordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8))),
-                          labelText: 'Password*',
-                        ),
-                      ),
-                    )),
+                  Flexible(
+                    child: Opacity(
+                        opacity: 0.3,
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          child: TextField(
+                            controller: lastNameController,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8))),
+                              labelText: 'Last Name',
+                            ),
+                          ),
+                        )),
+                  )
+                ],
               ),
-              Flexible(
-                child: Opacity(
-                    opacity: 0.3,
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      child: TextField(
-                        controller: confirmPasswordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          labelStyle: TextStyle(fontSize: 13),
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8))),
-                          labelText: 'Confirm Password*',
-                        ),
+              Opacity(
+                  opacity: 0.3,
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: TextField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8))),
+                        labelText: 'Email*',
                       ),
-                    )),
-              )
-            ],
-          ),
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Checkbox(
-                value: cValue,
-                onChanged: (bool? value) {
-                  setState(() {
-                    cValue = value!;
-                  });
-                },
+                    ),
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Flexible(
+                    child: Opacity(
+                        opacity: 0.3,
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          child: TextField(
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8))),
+                              labelText: 'Password*',
+                            ),
+                          ),
+                        )),
+                  ),
+                  Flexible(
+                    child: Opacity(
+                        opacity: 0.3,
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          child: TextField(
+                            controller: confirmPasswordController,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              labelStyle: TextStyle(fontSize: 13),
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8))),
+                              labelText: 'Confirm Password*',
+                            ),
+                          ),
+                        )),
+                  )
+                ],
               ),
-              const Text(
-                "I accept Terms & Conditions and Privacy Policies",
-                style: TextStyle(fontSize: 13),
-                textAlign: TextAlign.justify,
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Checkbox(
+                    value: cValue,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        cValue = value!;
+                      });
+                    },
+                  ),
+                  const Text(
+                    "I accept Terms & Conditions and Privacy Policies",
+                    style: TextStyle(fontSize: 13),
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
               ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Container(
-              height: 50,
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            side: const BorderSide(color: Color(0xff2cc48a))),
-                      ),
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xff2cc48a))),
-                  child: const Text('Sign Up',
-                      style: TextStyle(color: Colors.white)))),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text("Already have an account?"),
-              TextButton(
-                child: Text(
-                  'Sign In',
-                  style: TextStyle(
-                      fontSize: 17, color: Colors.black.withOpacity(0.6)),
-                ),
-                onPressed: () => Navigator.of(context).pop(),
-              )
-            ],
-          ),
-        ]));
+              const SizedBox(height: 10),
+              Container(
+                  height: 50,
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                side:
+                                    const BorderSide(color: Color(0xff2cc48a))),
+                          ),
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xff2cc48a))),
+                      child: const Text('Sign Up',
+                          style: TextStyle(color: Colors.white)))),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text("Already have an account?"),
+                  TextButton(
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                          fontSize: 17, color: Colors.black.withOpacity(0.6)),
+                    ),
+                    onPressed: () => Navigator.of(context).pop(),
+                  )
+                ],
+              ),
+            ])));
   }
 }
