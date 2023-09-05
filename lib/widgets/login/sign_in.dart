66ec4b64e0efee1c21b17f425396ea4c5ee38406
akshay-1705 <akshay.arora1705@gmail.com>
2023-09-06 00:49:15 +0530

    disable everything when clicked on login

--- lib/widgets/login/sign_in.dart
+++ lib/widgets/login/sign_in.dart
@@ -18,6 +18,7 @@ class _SignInWidgetState extends State<SignInWidget> {
   bool isLoading = false;
   String errorMessage = '';
   late AuthService authService;
+  bool isLoggingIn = false;
 
   @override
   void initState() {
@@ -42,173 +43,185 @@ class _SignInWidgetState extends State<SignInWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: GestureDetector(
-            onTap: () {
-              // Dismiss the keyboard when tapped on a non-actionable item
-              FocusScope.of(context).unfocus();
-            },
-            child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: ListView(
-                  children: <Widget>[
-                    const SizedBox(height: 60),
-                    Container(
-                        alignment: Alignment.center,
-                        padding: const EdgeInsets.all(10),
-                        child: const Text(
-                          'Sign In',
-                          style: TextStyle(
-                              color: Color(0xff000000),
-                              fontWeight: FontWeight.w400,
-                              fontSize: 30),
-                        )),
-                    Container(
-                      alignment: Alignment.center,
-                      child: const Text('To access the portal'),
-                    ),
-                    const SizedBox(height: 40),
-                    Center(
-                      child: Text(
-                        errorMessage,
-                        style: const TextStyle(color: Colors.red),
-                      ),
-                    ),
-                    Opacity(
-                        opacity: 0.5,
-                        child: Container(
-                          padding: const EdgeInsets.all(10),
-                          child: TextField(
-                            controller: emailController,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(8))),
-                              labelText: 'Email*',
+        body: AbsorbPointer(
+            absorbing: isLoggingIn,
+            child: GestureDetector(
+                onTap: () {
+                  // Dismiss the keyboard when tapped on a non-actionable item
+                  FocusScope.of(context).unfocus();
+                },
+                child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: ListView(
+                      children: <Widget>[
+                        const SizedBox(height: 60),
+                        Container(
+                            alignment: Alignment.center,
+                            padding: const EdgeInsets.all(10),
+                            child: const Text(
+                              'Sign In',
+                              style: TextStyle(
+                                  color: Color(0xff000000),
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 30),
+                            )),
+                        Container(
+                          alignment: Alignment.center,
+                          child: const Text('To access the portal'),
+                        ),
+                        const SizedBox(height: 40),
+                        Center(
+                          child: Text(
+                            errorMessage,
+                            style: const TextStyle(color: Colors.red),
+                          ),
+                        ),
+                        Opacity(
+                            opacity: 0.5,
+                            child: Container(
+                              padding: const EdgeInsets.all(10),
+                              child: TextField(
+                                controller: emailController,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(8))),
+                                  labelText: 'Email*',
+                                ),
+                              ),
+                            )),
+                        Opacity(
+                          opacity: 0.5,
+                          child: Container(
+                            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                            child: TextField(
+                              obscureText: true,
+                              controller: passwordController,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(8))),
+                                labelText: 'Password*',
+                                labelStyle: TextStyle(color: Color(0xff000000)),
+                              ),
+                              style: const TextStyle(color: Color(0xff000000)),
                             ),
                           ),
-                        )),
-                    Opacity(
-                      opacity: 0.5,
-                      child: Container(
-                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                        child: TextField(
-                          obscureText: true,
-                          controller: passwordController,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(8))),
-                            labelText: 'Password*',
-                            labelStyle: TextStyle(color: Color(0xff000000)),
-                          ),
-                          style: const TextStyle(color: Color(0xff000000)),
                         ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  const ResetPasswordWidget()),
-                        );
-                      },
-                      child: const Text('Forgot Password?',
-                          style: TextStyle(color: Colors.black)),
-                    ),
-                    const SizedBox(height: 10),
-                    Container(
-                        height: 50,
-                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                        child: isLoading
-                            ? const SizedBox(
-                                child:
-                                    Center(child: CircularProgressIndicator()),
-                              ) // Show loader while loading
-                            : ElevatedButton(
-                                onPressed: () async {
-                                  setState(() {
-                                    isLoading = true;
-                                    errorMessage = '';
-                                  });
-
-                                  final email = emailController.text;
-                                  final password = passwordController.text;
-
-                                  if (!isEmailValid(email)) {
-                                    setState(() {
-                                      errorMessage = 'Enter a valid email';
-                                      isLoading = false;
-                                    });
-                                    return;
-                                  } else if (!isPasswordValid(password)) {
-                                    setState(() {
-                                      errorMessage =
-                                          'Password must be at least 6 characters';
-                                      isLoading = false;
-                                    });
-                                    return;
-                                  }
-
-                                  try {
-                                    final response = await authService.login(
-                                      emailController.text,
-                                      passwordController.text,
-                                    );
-                                    print(response);
-
-                                    // If the server did return a 201 CREATED response,
-                                    // then parse the JSON.
-                                    print('Logged in successfully');
-                                  } catch (e) {
-                                    setState(() {
-                                      errorMessage =
-                                          'Invalid email or password.';
-                                    });
-                                  } finally {
-                                    setState(() {
-                                      isLoading = false;
-                                    });
-                                  }
-                                },
-                                style: ButtonStyle(
-                                    shape: MaterialStateProperty.all<
-                                        RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(8),
-                                          side: const BorderSide(
-                                              color: Color(0xff2cc48a))),
-                                    ),
-                                    backgroundColor:
-                                        MaterialStateProperty.all<Color>(
-                                            const Color(0xff2cc48a))),
-                                child: const Text('Login',
-                                    style: TextStyle(color: Colors.white)))),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const Text("Don't have an account?"),
+                        const SizedBox(height: 10),
                         TextButton(
-                          child: Text(
-                            'Sign Up',
-                            style: TextStyle(
-                                fontSize: 17,
-                                color: Colors.black.withOpacity(0.6)),
-                          ),
                           onPressed: () {
-                            Navigator.pushReplacement(
+                            Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => const SignUpWidget()),
+                                  builder: (context) =>
+                                      const ResetPasswordWidget()),
                             );
                           },
-                        )
+                          child: const Text('Forgot Password?',
+                              style: TextStyle(color: Colors.black)),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                            height: 50,
+                            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                            child: isLoading
+                                ? const SizedBox(
+                                    child: Center(
+                                        child: CircularProgressIndicator()),
+                                  ) // Show loader while loading
+                                : ElevatedButton(
+                                    onPressed: isLoggingIn
+                                        ? null // Disable the button when isLoggingIn is true
+                                        : () async {
+                                            setState(() {
+                                              isLoading = true;
+                                              errorMessage = '';
+                                              isLoggingIn = true;
+                                            });
+
+                                            final email = emailController.text;
+                                            final password =
+                                                passwordController.text;
+
+                                            if (!isEmailValid(email)) {
+                                              setState(() {
+                                                errorMessage =
+                                                    'Enter a valid email';
+                                                isLoading = false;
+                                              });
+                                              return;
+                                            } else if (!isPasswordValid(
+                                                password)) {
+                                              setState(() {
+                                                errorMessage =
+                                                    'Password must be at least 6 characters';
+                                                isLoading = false;
+                                              });
+                                              return;
+                                            }
+
+                                            try {
+                                              final response =
+                                                  await authService.login(
+                                                emailController.text,
+                                                passwordController.text,
+                                              );
+                                              print(response);
+                                              print('Logged in successfully');
+                                            } catch (e) {
+                                              setState(() {
+                                                errorMessage =
+                                                    'Invalid email or password.';
+                                              });
+                                            } finally {
+                                              setState(() {
+                                                isLoading = false;
+                                              });
+                                            }
+
+                                            setState(() {
+                                              isLoggingIn = false;
+                                            });
+                                          },
+                                    style: ButtonStyle(
+                                        shape: MaterialStateProperty.all<
+                                            RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(8),
+                                              side: const BorderSide(
+                                                  color: Color(0xff2cc48a))),
+                                        ),
+                                        backgroundColor:
+                                            MaterialStateProperty.all<Color>(
+                                                const Color(0xff2cc48a))),
+                                    child: const Text('Login',
+                                        style:
+                                            TextStyle(color: Colors.white)))),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const Text("Don't have an account?"),
+                            TextButton(
+                              child: Text(
+                                'Sign Up',
+                                style: TextStyle(
+                                    fontSize: 17,
+                                    color: Colors.black.withOpacity(0.6)),
+                              ),
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SignUpWidget()),
+                                );
+                              },
+                            )
+                          ],
+                        ),
                       ],
-                    ),
-                  ],
-                ))));
+                    )))));
   }
 }
