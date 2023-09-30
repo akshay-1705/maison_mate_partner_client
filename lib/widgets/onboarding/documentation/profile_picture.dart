18c4a29a7fa00c65d96eca7d18564f268c1961da
akshay-1705 <akshay.arora1705@gmail.com>
2023-09-30 21:27:36 +0530

    fetch profile picture wip

--- lib/widgets/onboarding/documentation/profile_picture.dart
+++ lib/widgets/onboarding/documentation/profile_picture.dart
@@ -1,4 +1,9 @@
+// import 'dart:convert';
+// import 'dart:io';
+
 import 'package:flutter/material.dart';
+// import 'package:maison_mate/constants.dart';
+import 'package:maison_mate/network/client/get_client.dart';
 import 'package:maison_mate/network/client/post_client.dart';
 import 'package:maison_mate/network/response/api_response.dart';
 import 'package:maison_mate/provider/documentation/profile_picture_model.dart';
@@ -6,6 +11,7 @@ import 'package:maison_mate/shared/custom_app_bar.dart';
 import 'package:maison_mate/shared/my_form.dart';
 import 'package:maison_mate/shared/my_snackbar.dart';
 import 'package:provider/provider.dart';
+// import 'package:path_provider/path_provider.dart';
 
 class ProfilePicture extends StatefulWidget {
   const ProfilePicture({Key? key}) : super(key: key);
@@ -17,58 +23,89 @@ class ProfilePicture extends StatefulWidget {
 class _ProfilePictureState extends State<ProfilePicture> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   Future<ApiResponse>? futureData;
+  late Future<ApiResponse> getFutureData;
+  // static const String apiUrl =
+  //     '$baseApiUrl/partners/onboarding/profile_picture';
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getFutureData = GetClient.fetchData(apiUrl);
+  //   // var model = Provider.of<ProfilePictureModel>(context, listen: false);
+  //   getFutureData.then((apiResponse) async {
+  //     if (mounted) {
+  //       if (apiResponse.data.profile_picture.isNotEmpty) {
+  //         var profilePicture = apiResponse.data.profile_picture;
+  //         var filename = apiResponse.data.file_name;
+  //         final imageBytes = base64Decode(profilePicture);
+  //         final tempDir = await getTemporaryDirectory();
+  //         final tempFile = File('${tempDir.path}/$filename');
+  //         await tempFile.writeAsBytes(imageBytes);
+  //         // model.setSelectedFile(tempFile);
+  //       }
+  //     }
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
         create: (context) => ProfilePictureModel(),
         child: Scaffold(
-          appBar: CustomAppBar.show(
-              context, "Profile picture", const Icon(Icons.arrow_back)),
-          body: WillPopScope(
-            onWillPop: () async {
-              return Future.value(false);
-            },
-            child: GestureDetector(
-              onTap: () {
-                FocusScope.of(context).unfocus();
+            appBar: CustomAppBar.show(
+                context, "Profile picture", const Icon(Icons.arrow_back)),
+            body: GetRequestFutureBuilder<dynamic>(
+              future: getFutureData,
+              builder: (context, data) {
+                return renderForm(context);
               },
-              child: SingleChildScrollView(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Consumer<ProfilePictureModel>(
-                      builder: (context, banking, child) {
-                    final ProfilePictureModel model =
-                        Provider.of<ProfilePictureModel>(context);
-                    return Form(
-                      key: _formKey,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          const SizedBox(height: 10),
-                          MyForm.formFieldHeader(
-                              'Attach a professional picture of yourself. No selfies or group photos; this is the photo customers will see when you book a job.*'),
-                          MyForm.uploadImageButton(model),
-                          const SizedBox(height: 20),
-                          (futureData != null)
-                              ? PostClient.futureBuilder(
-                                  model,
-                                  futureData!,
-                                  "Submit",
-                                  () async {
-                                    onSubmitCallback(model);
-                                  },
-                                  () {},
-                                )
-                              : MyForm.submitButton("Submit", () async {
-                                  onSubmitCallback(model);
-                                }),
-                        ],
-                      ),
-                    );
-                  })),
-            ),
-          ),
-        ));
+            )));
+  }
+
+  WillPopScope renderForm(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        return Future.value(false);
+      },
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+            padding: const EdgeInsets.all(5.0),
+            child: Consumer<ProfilePictureModel>(
+                builder: (context, banking, child) {
+              final ProfilePictureModel model =
+                  Provider.of<ProfilePictureModel>(context);
+              return Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 10),
+                    MyForm.formFieldHeader(
+                        'Attach a professional picture of yourself. No selfies or group photos; this is the photo customers will see when you book a job.*'),
+                    MyForm.uploadImageSection(model),
+                    const SizedBox(height: 20),
+                    (futureData != null)
+                        ? PostClient.futureBuilder(
+                            model,
+                            futureData!,
+                            "Submit",
+                            () async {
+                              onSubmitCallback(model);
+                            },
+                            () {},
+                          )
+                        : MyForm.submitButton("Submit", () async {
+                            onSubmitCallback(model);
+                          }),
+                  ],
+                ),
+              );
+            })),
+      ),
+    );
   }
 
   void onSubmitCallback(ProfilePictureModel model) {
